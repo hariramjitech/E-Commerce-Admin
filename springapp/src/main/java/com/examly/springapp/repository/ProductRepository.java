ecd7c8e133f5e95bce4129eb9a3acf257bdf4331
hariramjitech <hariramji.3423n@gmail.com>
2025-08-05 14:40:55 +0000

    done

--- springapp/src/main/java/com/examly/springapp/repository/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.examly.springapp.repository;
-
-import com.examly.springapp.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {}
