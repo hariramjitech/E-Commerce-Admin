fb99c8f2876da7ab1467ee5abbd321039132a8bd
hariramjitech <hariramji.3423n@gmail.com>
2025-08-05 09:50:50 +0000

    done

--- springapp/src/main/java/com/examly/springapp/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.examly.springapp.repository;
-
-import com.examly.springapp.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {}
