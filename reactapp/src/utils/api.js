99fd65938a3ec1dddb224579fcf0aa188981636f
Hariramji H <hariramji.3423n@gmail.com>
2025-08-05 22:16:04 +0530

    front

--- reactapp/src/utils/api.js
+++ reactapp/src/utils/api.js
@@ -1,13 +1,26 @@
+// src/utils/api.js
 import axios from 'axios';
 
 const BASE_URL = 'https://8080-becabbbccbbfdfebdfdfccaeabdfcdafc.premiumproject.examly.io/api';
 
-export const fetchProducts = () => axios.get(`${BASE_URL}/products`);
+// 🔁 Products
+export const fetchProducts = (filters = {}) => {
+  const params = new URLSearchParams();
+
+  if (filters.category) params.append('category', filters.category);
+  if (filters.minPrice) params.append('minPrice', filters.minPrice);
+  if (filters.maxPrice) params.append('maxPrice', filters.maxPrice);
+  if (filters.stockStatus && filters.stockStatus !== 'all') params.append('stockStatus', filters.stockStatus);
+
+  return axios.get(`${BASE_URL}/products?${params.toString()}`);
+};
+
 export const getProduct = (id) => axios.get(`${BASE_URL}/products/${id}`);
 export const createProduct = (data) => axios.post(`${BASE_URL}/products`, data);
 export const updateProduct = (id, data) => axios.put(`${BASE_URL}/products/${id}`, data);
 export const deleteProduct = (id) => axios.delete(`${BASE_URL}/products/${id}`);
 
+// 🔁 Orders
 export const fetchOrders = () => axios.get(`${BASE_URL}/orders`);
 export const getOrder = (id) => axios.get(`${BASE_URL}/orders/${id}`);
 export const createOrder = (data) => axios.post(`${BASE_URL}/orders`, data);
